32ae4aaff2a003f2e84d9e8cbccc4c6baa4401d7
Arie van der Poel <arie@mediasuite.co.nz>
2021-03-23 21:27:58 +1300

    Coding excercise 23 March

---
+++ code_with_phil.js
@@ -0,0 +1,25 @@
+// Psudo code
+// Problem 1
+
+const input = [true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]
+
+const numberOfSheep = input.filter(sheep => {
+  return sheep === true
+}).length
+
+// Problem 2
+
+function highAndLowAttempt1(numbers){
+  const arrayNum = numbers.split(" ").map(num => {
+    return parseInt(num)
+  }).filter(Boolean)
+
+  return `${Math.max.apply(null, arrayNum)} ${Math.min.apply(null, arrayNum)}`
+}
+  
+highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
