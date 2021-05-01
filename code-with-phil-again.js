b3022e2592f2d5bdadf43456b48921bbab3356d5
Arie van der Poel <arie@mediasuite.co.nz>
2021-05-01 23:44:32 +1200

    Coding again

--- code-with-phil-again.js
+++ code-with-phil-again.js
@@ -1,3 +1,28 @@
+function deathStar(week) {
+  var message = "The station is completed!"
+  const materials = [100, 75, 50]
+  const shipments = [0, 0, 0]
+
+  week.forEach((day, index) => {
+    if (index >= week[week.length -1]) { return }
+    day.forEach((n, i) => {
+      shipments[i] += n
+    })
+  })
+
+  shipments.forEach((s, i) => {
+    if (s < materials[i]) {
+       message = `The station is destroyed! It needed ${materials[0] - shipments[0]} iron, ${materials[1] - shipments[1]} steel and ${materials[2] - shipments[2]} chromium for completion.`
+    }
+  })
+
+  return message
+}
+
+const week = [[100,75,49],[20,15,20],[10,15,10],[50,50,20],[20,15,10],[20,15,10],[20,15,10],3]
+
+deathStar([[100,75,49],[20,15,20],[10,15,10],[50,50,20],[20,15,10],[20,15,10],[20,15,10],3])
+
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 // solution('abc', 'bc') // returns true
